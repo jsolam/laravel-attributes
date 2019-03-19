d5ca382e92dc25d7560138774e5a16aac30eeb53
jsolam <34037044+jsolam@users.noreply.github.com>
2019-03-19 17:22:58 +0100

    Update Attributable.php

--- src/Traits/Attributable.php
+++ src/Traits/Attributable.php
@@ -158,7 +158,7 @@ trait Attributable
         static::$entityAttributes = static::$entityAttributes ?? collect();
 
         if (! static::$entityAttributes->has($morphClass) && Schema::hasTable(config('rinvex.attributes.tables.attribute_entity'))) {
-            $locale = app()->getLocale();
+            $locale = app('translator')->getLocale();
 
             /* This is a trial to implement per resource attributes,
                it's working but I don't like current implementation.
